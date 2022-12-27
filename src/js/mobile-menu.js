d2a41a37042bfea109429550a94ec970c20709c3
konstantin <kosvadmail@atomlc.com.ua>
2022-12-27 23:32:40 +0100

    Добавляет часть мобильной версии

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,5 +1,6 @@
 const mainNavSandwich = document.querySelector('.main-header__mobile-menu-button');
 const mainNav = document.querySelector('.main-header__mobile-menu-container');
+const page = document.querySelector('.page');
 
 export const startMobileMenu = () => {
   if (mainNavSandwich) {
@@ -9,12 +10,16 @@ export const startMobileMenu = () => {
         mainNavSandwich.classList.add('main-header__mobile-menu-button--opened');
         mainNav.classList.remove('main-header__mobile-menu-container--closed');
         mainNav.classList.add('main-header__mobile-menu-container--opened');
+        page.classList.add('page--opened-mobile-menu');
+        page.classList.remove('page--closed-mobile-menu');
       } else {
         mainNavSandwich.classList.remove('main-header__mobile-menu-button--opened');
         mainNavSandwich.classList.add('main-header__mobile-menu-button--closed-animation');
 
         mainNav.classList.add('main-header__mobile-menu-container--closed');
         mainNav.classList.remove('main-header__mobile-menu-container--opened');
+        page.classList.remove('page--opened-mobile-menu');
+        page.classList.add('page--closed-mobile-menu');
       }
     });
   }
